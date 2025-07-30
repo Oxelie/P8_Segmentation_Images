30c46cbc1fb086078dc220fbe68226a14a32505e
Oxelie <stephanieduhem.simplon@gmail.com>
2025-07-30 22:18:21 +0200

    maj pythonpath

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,31 +1,26 @@
-import pytest
-from api import app
-
-
-import requests
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from api import app
 
+import pytest
+import requests
+
 def test_predict_endpoint():
     url = "http://localhost:5000/predict"
-    # Utilise une petite image de test
     with open("tests/test_image.png", "rb") as img:
         files = {"image": img}
         response = requests.post(url, files=files)
     assert response.status_code == 200
     data = response.json()
     assert "mask" in data
-    
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
-def test_predict_endpoint(client):
-    # Simule l'envoi d'une image à l'API
+def test_predict_endpoint_flask(client):
     with open("tests/test_image.png", "rb") as img:
         response = client.post("/predict", data={"image": img})
     assert response.status_code == 200
