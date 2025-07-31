862e50f6dc6971ec47681a2a9e7c930ffbe9ab27
Oxelie <stephanieduhem.simplon@gmail.com>
2025-07-31 09:10:12 +0200

    modif import api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from api import app
+from api.api import app
 
 import pytest
 import requests
